c159e24001ebe07ff031652923f56e3ee8b25d04
Maxim Manaenkov <manaenkov2000@gmail.com>
2020-01-21 21:12:38 +0300

    Updates

---
+++ precache-manifest.fb00da5fe7e70a5728bad6c245422621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e948b3afe9b03a883cb0",
+    "url": "/meets/static/js/runtime~main.edbc2184.js"
+  },
+  {
+    "revision": "add86427b8a33ef6eed4",
+    "url": "/meets/static/js/main.2caa7787.chunk.js"
+  },
+  {
+    "revision": "b5db9d70ba355cb1b2f1",
+    "url": "/meets/static/js/2.46a34108.chunk.js"
+  },
+  {
+    "revision": "add86427b8a33ef6eed4",
+    "url": "/meets/static/css/main.db44005f.chunk.css"
+  },
+  {
+    "revision": "b5db9d70ba355cb1b2f1",
+    "url": "/meets/static/css/2.9b1f0dfb.chunk.css"
+  },
+  {
+    "revision": "6ec767e7be50591bc55f76cb1a6ff27a",
+    "url": "/meets/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/meets/precache-manifest.2938d580eb04a93072438ddd1c0c8097.js"
+  "/meets/precache-manifest.fb00da5fe7e70a5728bad6c245422621.js"
 );
 
 workbox.clientsClaim();
