fa9947ef2a2a806c33b1a7e86fc4efde473e491b
Maxim Manaenkov <manaenkov2000@gmail.com>
2020-01-22 00:06:28 +0300

    Updates

--- precache-manifest.7405800400232bc65ea85bf99f210c24.js
+++ precache-manifest.7405800400232bc65ea85bf99f210c24.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/meets/static/js/runtime~main.edbc2184.js"
   },
   {
-    "revision": "6e3cafb9cf3b2f815b88",
-    "url": "/meets/static/js/main.bf6d8baa.chunk.js"
+    "revision": "46e45113cba33e5647c5",
+    "url": "/meets/static/js/main.a9ca48a0.chunk.js"
   },
   {
     "revision": "488d525f7e70e15a80bb",
     "url": "/meets/static/js/2.d7fdb55e.chunk.js"
   },
   {
-    "revision": "6e3cafb9cf3b2f815b88",
+    "revision": "46e45113cba33e5647c5",
     "url": "/meets/static/css/main.db44005f.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/meets/static/css/2.9b1f0dfb.chunk.css"
   },
   {
-    "revision": "968eff30422b749d549336a89ec3193b",
+    "revision": "815c33b7d07e3a1bd8c86858bd75daff",
     "url": "/meets/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/meets/precache-manifest.a21304ad44f829ed052453d4b3709973.js"
+  "/meets/precache-manifest.7405800400232bc65ea85bf99f210c24.js"
 );
 
 workbox.clientsClaim();
